8dafc1996fdd6d0df3faaa0fb43a5132e503853c
saadsabir <saadsabir9@gmail.com>
2023-07-18 15:13:19 +0200

    update app router

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./layout/login/login.component";
+import { LoginComponent } from "./login/login.component";
 import { LayoutComponent } from "./layout/layout.component";
+
 const routes: Routes = [
     {
         path: 'login',
